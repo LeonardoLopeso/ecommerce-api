28d81f2a552e1d96db4a6a375cd2aedd135be345
LeonardoLopeso <l.lopes355@gmail.com>
2024-12-30 14:36:25 -0300

    fix: token valid

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,13 +1,23 @@
 import express, { NextFunction, Request, Response } from "express";
 import { UnauthorizedError } from "../errors/unauthorized.error";
+import { DecodedIdToken, getAuth } from "firebase-admin/auth";
 
 export const auth = (app: express.Express) => {
-  app.use((req: Request, res: Response, next: NextFunction) => {
+  app.use(async (req: Request, res: Response, next: NextFunction) => {
+    if (req.method === "POST" && req.url.endsWith("/auth/login")) {
+      return next();
+    }
+
     const token = req.headers.authorization?.split("Bearer ")[1];
 
     if (token) {
-      console.log('[TOKEN]: ', token);
-      return next();
+      try {
+        const decodeIdToken: DecodedIdToken = await getAuth().verifyIdToken(token, true)
+        console.log('[DECODED_ID_TOKEN]: ', decodeIdToken);
+        return next();
+      } catch (error) {
+        next(new UnauthorizedError());
+      }
     }
 
     next(new UnauthorizedError());
