86d3e04c8af501d947402f431fd4bf249d26b160
LeonardoLopeso <l.lopes355@gmail.com>
2024-12-30 14:48:16 -0300

    feat: add logged in user to request

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,6 +1,8 @@
 import express, { NextFunction, Request, Response } from "express";
 import { UnauthorizedError } from "../errors/unauthorized.error";
 import { DecodedIdToken, getAuth } from "firebase-admin/auth";
+import { UserService } from "../services/use.service";
+import { ForbiddenError } from "../errors/forbidden.error";
 
 export const auth = (app: express.Express) => {
   app.use(async (req: Request, res: Response, next: NextFunction) => {
@@ -13,7 +15,14 @@ export const auth = (app: express.Express) => {
     if (token) {
       try {
         const decodeIdToken: DecodedIdToken = await getAuth().verifyIdToken(token, true)
-        console.log('[DECODED_ID_TOKEN]: ', decodeIdToken);
+        
+        const user = await new UserService().getById(decodeIdToken.uid);
+
+        if (!user) {
+          return next(new ForbiddenError());
+        }
+        req.user = user;
+
         return next();
       } catch (error) {
         next(new UnauthorizedError());
